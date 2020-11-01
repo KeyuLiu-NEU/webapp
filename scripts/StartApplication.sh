0f017b98151d0c47cc62bf45af3d826d48ec52c3
Keyu Liu <keyuliu@KeyudeMacBook-Pro.local>
2020-11-01 17:16:11 -0500

    test

--- scripts/StartApplication.sh
+++ scripts/StartApplication.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu
-java -jar CloudNativeWebApp-0.0.1-SNAPSHOT.jar
+setsid java -jar CloudNativeWebApp-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
--- scripts/StopApplication.sh
+++ scripts/StopApplication.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu
 rm CloudNativeWebApp-0.0.1-SNAPSHOT.jar
+sudo fuser -k 8080/tcp
+if [  -f "CloudNativeWebApp-0.0.1-SNAPSHOT.jar" ];then
+rm CloudNativeWebApp-0.0.1-SNAPSHOT.jar
+fi
